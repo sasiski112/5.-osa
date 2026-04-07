a05257e78fcd1289cee4deede2112e93693d278c
sasiski112 <mshevelovv@gmail.com>
2026-04-07 12:32:51 +0300

    Refactor StartPage class and update menu options

--- StartPage.cs
+++ StartPage.cs
@@ -1,14 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
-namespace _5._osa
+class StartPage
 {
-    internal class StartPage
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Program.Wkers();
+            Console.WriteLine("\n=== MENÜÜ ===");
+            Console.WriteLine("1 - Kalorite kalkulaator");
+            Console.WriteLine("2 - Maakonnad ja pealinnad");
+            Console.WriteLine("3 - Õpilased ja hinded");
+            Console.WriteLine("4 - Filmide kogu");
+            Console.WriteLine("5 - Massiivi statistika");
+            Console.WriteLine("6 - Lemmikloomad");
+            Console.WriteLine("7 - Valuutakalkulaator");
+            Console.WriteLine("0 - Välju");
+
+            Console.Write("\nValik: ");
+            string valik = Console.ReadLine();
+
+            switch (valik)
+            {
+                case "1":
+                    Alamfunktsioonid.Calculator();
+                    break;
+                case "2":
+                    Alamfunktsioonid.MaakonnadJaPealinnad();
+                    break;
+                case "3":
+                    Alamfunktsioonid.OpilasedJaHinded();
+                    break;
+                case "4":
+                    Alamfunktsioonid.FilmideKogu();
+                    break;
+                case "5":
+                    Alamfunktsioonid.MassiiviStatistika();
+                    break;
+                case "6":
+                    Alamfunktsioonid.Lemmikloomad();
+                    break;
+                case "7":
+                    Alamfunktsioonid.ValuutaKalkulaator();
+                    break;
+                case "0":
+                    return;
+                default:
+                    Console.WriteLine("Vale valik!");
+                    break;
+            }
         }
     }
 }
