f287c7fd121bae204f88af3437aca8dab6da8928
sasiski112 <mshevelovv@gmail.com>
2026-04-19 21:42:58 +0300

    Refactor menu options and update function calls

--- StartPage.cs
+++ StartPage.cs
@@ -1,48 +1,60 @@
 using System;
 
-class StartPage
+class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         while (true)
         {
-            Console.WriteLine("\n=== MENÜÜ ===");
-            Console.WriteLine("1 - Kalorite kalkulaator");
-            Console.WriteLine("2 - Maakonnad ja pealinnad");
-            Console.WriteLine("3 - Õpilased ja hinded");
-            Console.WriteLine("4 - Filmide kogu");
-            Console.WriteLine("5 - Massiivi statistika");
-            Console.WriteLine("6 - Lemmikloomad");
-            Console.WriteLine("7 - Valuutakalkulaator");
-            Console.WriteLine("0 - Välju");
+            Console.WriteLine("\nVali tegemus:");
+            Console.WriteLine("1. Kalorite kalkulaator");
+            Console.WriteLine("2. Maakonnad ja pealinnad");
+            Console.WriteLine("3. Õpilaste ja toodete analüüs");
+            Console.WriteLine("4. Filmihaldus");
+            Console.WriteLine("5. Arvude statistika");
+            Console.WriteLine("6. Lemmikloomade registreerimine");
+            Console.WriteLine("7. Valuuta kalkulaator");
+            Console.WriteLine("0. Välju programmist");
+            Console.Write("\nSinu valik: ");
 
-            Console.Write("\nValik: ");
             string valik = Console.ReadLine();
+
 
             switch (valik)
             {
                 case "1":
-                    Alamfunktsioonid.Calculator();
+                    Console.Clear();
+                    Alamfunktsioonid.CaloorideCalculator();
                     break;
                 case "2":
+                    Console.Clear();
                     Alamfunktsioonid.MaakonnadJaPealinnad();
                     break;
                 case "3":
-                    Alamfunktsioonid.OpilasedJaHinded();
+                    Console.Clear();
+                    Alamfunktsioonid.OpilasedJaTooted();
                     break;
                 case "4":
-                    Alamfunktsioonid.FilmideKogu();
+                    Console.Clear();
+                    Alamfunktsioonid.FilmiHaldus();
                     break;
                 case "5":
-                    Alamfunktsioonid.MassiiviStatistika();
+                    Console.Clear();
+                    Alamfunktsioonid.ArvudeStatistika();
                     break;
                 case "6":
-                    Alamfunktsioonid.Lemmikloomad();
+                    Console.Clear();
+                    Alamfunktsioonid.LemmikloomaHaldus();
                     break;
                 case "7":
+                    Console.Clear();
                     Alamfunktsioonid.ValuutaKalkulaator();
                     break;
                 case "0":
+                    Console.Clear();
+                    Console.WriteLine("Head aega!");
                     return;
                 default:
                     Console.WriteLine("Vale valik!");
